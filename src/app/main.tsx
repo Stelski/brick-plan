b8ff7419fb7daa8f2d211bfe404f2a41bdee9af7
송윤남 <ynsong@hanatour.com>
2025-06-09 16:17:35 +0900

    [SYN] 경로 설정 @ /src 처리

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+//import App from './App.tsx'
+import { Pages } from '@/pages'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Pages />
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: [
-      { find: '@', replacement: '/src' } // This alias allows you to use '@' to refer to the 'src' directory  { '@': '/src'  }
+      { find: '@', replacement: path.resolve(__dirname, './src/') } 
     ]
   }
 })
